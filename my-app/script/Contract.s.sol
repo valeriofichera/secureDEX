738c1b1ab2088d660971ccedcc27bac129e65755
Valerio <78494380+valeriofichera@users.noreply.github.com>
2023-11-18 20:49:27 +0100

    set up foundry

---
+++ my-app/script/Contract.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import { HelloWorld } from '../src/HelloWorld.sol';
+
+contract ContractScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+        new HelloWorld("Hello from Foundry!");
+    }
+}
+
+cast send $CONTRACT_ADDRESS "updateGreeting(string)" "Penis" --private-key $PRIVATE_KEY
